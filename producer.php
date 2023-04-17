3738cc9068283c89ae49b65dd72a99139a68475f
electi <barankircaa@gmail.com>
2023-04-17 13:59:00 +0300

    connect producer to db

--- producer.php
+++ producer.php
@@ -1,29 +1,47 @@
 <!DOCTYPE php>
+<?php
+$conn = mysqli_connect('localhost', 'algos', '123456', 'dbtest');
+
+//write query for all infos
+$sql = 'SELECT light, lightColor, tempature, sound, securityS, door, roomba, waterHeater FROM device';
+
+//make query and get result
+$result = mysqli_query($conn, $sql);
+
+//fetch the resulting rows as an array
+$infos = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//frees result from memory for good practice
+mysqli_free_result($result);
+
+print_r($infos);
+?>
+
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AutoHome</title>
+    <title>AutoHome-Producer</title>
     <link rel="stylesheet" href="producer-styles.css">
 </head>
 
 <body>
     <div class="header">
-        <h1>Dashboard</h1>
+        <h1>Producer Dashboard</h1>
     </div>
     <div class="cards">
         <div class="card">
-            <p>LIGHTS</p>
-            <p>OFF</p>
+            <p class="title">LIGHTS</p>
+            <p class="state">OFF</p>
             <form action="" class="light-control" method="post">
                 <button type="submit" name="light">TURN ON</button>
             </form>
         </div>
         <div class="card">
-            <p>LIGHT COLOR</p>
-            <p>GREEN</p>
+            <p class="title">LIGHT COLOR</p>
+            <p class="state">GREEN</p>
             <form action="" class="color-control" method="post">
                 <div>
                     <label for="red">RED</label>
@@ -41,26 +59,26 @@
             </form>
         </div>
         <div class="card">
-            <p>DESIRED TEMPERATURE</p>
+            <p class="title">DESIRED TEMPERATURE</p>
             <form action="" method="post"
                 style="display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 20px;">
                 <div class="temp-control">
                     <button>-</button>
-                    <p data-value="45">45 C°</p>
+                    <p class="state" data-value="45">45 C°</p>
                     <button>+</button>
                 </div>
                 <button type="submit" name="air">START AIR CONDITIONER</button>
             </form>
         </div>
         <div class="card">
-            <p>SOUND SYSTEM</p>
-            <p>ON</p>
+            <p class="title">SOUND SYSTEM</p>
+            <p class="state">ON</p>
             <form action="" method="post">
                 <button type="submit" name="sound">TURN OFF</button>
             </form>
         </div>
         <div class="card">
-            <p>SECURITY</p>
+            <p class="title">SECURITY</p>
             <form action="" class="security-control" method="post">
                 <div> <label for="low">LOW</label>
                     <input type="radio" name="security-level" id="low">
@@ -75,8 +93,8 @@
             </form>
         </div>
         <div class="card">
-            <p>DOOR LOCK</p>
-            <p>LOCKED</p>
+            <p class="title">DOOR LOCK</p>
+            <p class="state">LOCKED</p>
             <form action="" method="post">
                 <button type="submit" name="lock">UNLOCK</button>
             </form>
